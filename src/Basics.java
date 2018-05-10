815b3847561e2a78a8417d31fbe625fb117809e5
Clarencio Tan <clarenciotan@gmail.com>
2018-05-10 16:10:45 +1200

    end of "Automating app UI Using Xpaths and text attribute" episode

--- src/Basics.java
+++ src/Basics.java
@@ -1,17 +1,25 @@
 import java.net.MalformedURLException;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class Basics {
-	
 
 	public static void main(String[] args) throws MalformedURLException {
 		AndroidDriver<AndroidElement> driver;
 		driver = Base.Capabilities();
-		
-//		driver.
-		
+
+		// xpath, id, className (css selector) and androidUIAutomator
+
+		// driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		// //works!
+		new WebDriverWait(driver, 10).until(
+				ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.TextView[@text='Preference']")))
+				.click(); // works as well!
 
 	}
 
