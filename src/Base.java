40dee69f7bc934d1402be6261181749387923731
Clarencio Tan <clarenciotan@gmail.com>
2018-05-10 15:02:56 +1200

    end of "UIAutomator tool usage - Inspection of elements" episode

--- src/Base.java
+++ src/Base.java
@@ -10,24 +10,26 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class Base {
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
 		// TODO Auto-generated method stub
-		
+		AndroidDriver<AndroidElement> driver;
+
 		File holder = new File("src");
 		File app = new File(holder, "ApiDemos-debug.apk");
-		
+
 		DesiredCapabilities caps = new DesiredCapabilities();
 		// belows for my emulator
 		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus_5_API_24_1");
-		
+
 		// belows for my Sony Xperia XA1
-//		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
-//		caps.setCapability(MobileCapabilityType.UDID, "RQ3005MHH5");
-        caps.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-        
-        AndroidDriver<AndroidElement> ad = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
-        
-        
+		// caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+		// caps.setCapability(MobileCapabilityType.UDID, "RQ3005MHH5");
+		caps.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+
+		driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps); 
+		return driver;
+		// at this end, appium will wait for additional command for 6 sec
+
 	}
 
 }
---
+++ src/Basics.java
@@ -0,0 +1,18 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class Basics {
+	
+
+	public static void main(String[] args) throws MalformedURLException {
+		AndroidDriver<AndroidElement> driver;
+		driver = Base.Capabilities();
+		
+//		driver.
+		
+
+	}
+
+}
