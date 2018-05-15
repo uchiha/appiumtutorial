161041db81f2041eeb97fb8e3808ae1a1085fd46
Clarencio Tan <clarenciotan@gmail.com>
2018-05-15 13:47:34 +1200

    end of "Example on package name and activity invoking apps" episode. Note that this was run on a real device.

--- src/Base.java
+++ src/Base.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
 
 public class Base {
 
 	public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
-		// TODO Auto-generated method stub
+		
 		AndroidDriver<AndroidElement> driver;
 
 		File holder = new File("src");
@@ -30,6 +32,22 @@ public class Base {
 		return driver;
 		// at this end, appium will wait for additional command for 6 sec
 
+	}
+	
+	public static AndroidDriver<AndroidElement> atMobileApp() throws MalformedURLException {
+		AndroidDriver<AndroidElement> driver;
+
+		// NOTE: https://discuss.appium.io/t/app-activity-is-not-available-in-mobilecapabilitytype/10691/2
+
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
+		caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "25");
+		caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "nz.govt.at.atmobile");
+		caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "nz.govt.at.atmobile.MainActivity");
+
+		driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps); 
+		return driver;
 	}
 
 }
